81dd1b2d808cdacbdc7d245ec313efa435b5b458
Torreythms <torreythms@gmail.com>
2020-06-20 14:54:42 -0400

    I completed the navigation menu sans actual page id links

--- dowtax/src/App.js
+++ dowtax/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <li className="nav-item"><a href="#">Home</a></li>
         <li className="nav-item"><a href="#">About</a></li>
         <li className="nav-item"><a href="#">Services</a></li>
-        <li className="nav-item"><a href="#">Dropdown</a></li>
+        <li className="nav-item"><a href="#">Updates</a></li>
         <li className="nav-item"><a href="#">Payment</a></li>
         <li className="nav-item"><a href="#">Contact</a></li>
         </ul>
