460544d65922d5671f156d7b0c75d92e64184515
Torreythms <torreythms@gmail.com>
2020-06-20 14:46:19 -0400

    navigation styling

--- dowtax/src/App.js
+++ dowtax/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Navigation/Navigation';
 import 'tachyons';
 
 function App() {
   return (
     <div className="App">
-<Navbar/>
+      <div className="overlay">
+      <header className="navigation-header">
+        <ul className="navigation-items-container">
+        <li className="nav-item"><a href="#">Home</a></li>
+        <li className="nav-item"><a href="#">About</a></li>
+        <li className="nav-item"><a href="#">Services</a></li>
+        <li className="nav-item"><a href="#">Dropdown</a></li>
+        <li className="nav-item"><a href="#">Payment</a></li>
+        <li className="nav-item"><a href="#">Contact</a></li>
+        </ul>
+      </header>
+      </div>
     </div>
+  
   );
 }
 
